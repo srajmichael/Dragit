708a6130eb09bfcc1548ac41386b25ecbcfd49ec
Mike <michaelsrajjr@gmail.com>
2020-06-07 15:34:24 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/dragit.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+
+}
